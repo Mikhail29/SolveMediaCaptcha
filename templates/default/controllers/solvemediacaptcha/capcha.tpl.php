b6138ee0ce42f647d67e5b710205b53666cbe78e
Михаил Михайлов <mixajlov.mikhail92@yandex.com>
2017-02-03 04:08:22 +0200

    Фикс мобильной версии
    Signed-off-by: Михаил Михайлов <mixajlov.mikhail92@yandex.com>

--- templates/default/controllers/solvemediacaptcha/capcha.tpl.php
+++ templates/default/controllers/solvemediacaptcha/capcha.tpl.php
@@ -1,3 +1,4 @@
+<?php $this->addCSS("templates/{$this->name}/controllers/solvemediacaptcha/css/captchafix.css"); ?>
 <script type="text/javascript">
 	var ACPuzzleOptions = {
 		theme:	'<?php echo $this->controller->options['theme'] ?>'
